016f38798e51c3faceca96a9f2d25b47132253d6
Lothaire07 <lothaire.chacornac@epita.fr>
2025-01-31 14:59:06 +0100

    open file in bin + save + rand namegen for bin

---
+++ src/backend/file_manager/file_handling.rs
@@ -0,0 +1,28 @@
+use rand::Rng;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::Path;
+
+fn binary_namegen() -> String {
+    let mut rng = rand::thread_rng();
+    let id = rng.gen();
+    format!("bin_{}.v", id)
+}
+
+fn open_file_binary(path: &Path) -> Vec<u8> {
+    let mut file = File::open(path).unwrap();
+    let mut contents = Vec::new();
+    file.read_to_end(&mut contents).unwrap();
+    contents
+}
+
+fn save_binary(contents: &[u8]) {
+    let mut file = File::create(
+        std::env::current_dir()
+            .unwrap()
+            .join("binary_files")
+            .join("bin".join(binary_namegen())),
+    )
+    .unwrap();
+    file.write_all(contents).unwrap();
+}
