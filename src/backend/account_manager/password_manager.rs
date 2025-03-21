effb547b3a24b4f0910c89f30c41f563a3b34913
matevl <144525314+matevl@users.noreply.github.com>
2025-03-21 13:15:40 +0100

    Update password_manager.rs

--- src/backend/account_manager/password_manager.rs
+++ src/backend/account_manager/password_manager.rs
@@ -1,21 +1,21 @@
-use std::collections::HashMap;
-use std::fs;
-use std::sync::{Arc, Mutex};
+use crate::backend::account_manager::account_server::{UserJson, JWT, PRIVATE_DATA, ROOT};
+use crate::backend::aes_keys::crypted_key::encrypt;
+use crate::backend::{PASSWORD, VAULTIFY_DATABASE, VAULTS_DATA};
 use actix_web;
 use actix_web::{web, HttpResponse, Responder};
 use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 use sha2::digest::typenum::private::IsNotEqualPrivate;
-use crate::backend::account_manager::account_server::{UserJson, JWT, PRIVATE_DATA, ROOT};
-use crate::backend::{PASSWORD, VAULTIFY_DATABASE, VAULTS_DATA};
-use crate::backend::aes_keys::crypted_key::encrypt;
+use std::collections::HashMap;
+use std::fs;
+use std::sync::{Arc, Mutex};
 
-lazy_static!(
+lazy_static! {
     static ref PASSWORD_MANAGER: Arc<Mutex<PasswordManager>> = {
-        let mut manager = PasswordManager::new();
+        let manager = PasswordManager::new();
         Arc::new(Mutex::new(manager))
     };
-);
+}
 
 type PasswordManager = HashMap<u32, Vec<Password>>;
 
@@ -28,17 +28,25 @@ pub struct Password {
 }
 
 pub async fn get_passwords(jwt: web::Json<JWT>) -> impl Responder {
-    let data = PASSWORD_MANAGER.lock().unwrap().get(&jwt.id).unwrap();
+    let password_manager = PASSWORD_MANAGER.lock().unwrap();
+    let data = password_manager.get(&jwt.id).unwrap();
     HttpResponse::Ok().json(data.clone())
 }
 pub async fn add_passwords(jwt: web::Json<JWT>, password: web::Json<Password>) -> impl Responder {
-    let data = PASSWORD_MANAGER.lock().unwrap().get_mut(&jwt.id).unwrap();
+    let mut password_manager = PASSWORD_MANAGER.lock().unwrap();
+    let data = password_manager.get_mut(&jwt.id).unwrap();
+
     data.push(password.clone());
     HttpResponse::Ok().json(data.clone())
 }
 
-pub async fn remove_passwords(jwt: web::Json<JWT>, password: web::Json<Password>) -> impl Responder {
-    let data = PASSWORD_MANAGER.lock().unwrap().get_mut(&jwt.id).unwrap();
+pub async fn remove_passwords(
+    jwt: web::Json<JWT>,
+    password: web::Json<Password>,
+) -> impl Responder {
+    let mut password_manager = PASSWORD_MANAGER.lock().unwrap();
+    let data = password_manager.get_mut(&jwt.id).unwrap();
+
     let clone = data.clone();
     data.clear();
     for c in clone {
@@ -50,11 +58,19 @@ pub async fn remove_passwords(jwt: web::Json<JWT>, password: web::Json<Password>
 }
 
 pub async fn save_password(jwt: web::Json<JWT>) -> impl Responder {
-    let data = PASSWORD_MANAGER.lock().unwrap().get(&jwt.id).unwrap();
+    let password_manager = PASSWORD_MANAGER.lock().unwrap();
+    let data = password_manager.get(&jwt.id).unwrap();
+
     let private_datas = PRIVATE_DATA.lock().unwrap();
     let private_data = private_datas.get(&jwt.id).unwrap();
 
-    let path = format!("{}{}{}/{}", ROOT.to_str().unwrap(), VAULTS_DATA, jwt.id, PASSWORD);
+    let path = format!(
+        "{}{}{}/{}",
+        ROOT.to_str().unwrap(),
+        VAULTS_DATA,
+        jwt.id,
+        PASSWORD
+    );
 
     let contend = serde_json::to_string(&data).unwrap();
     let encrypted_contend = encrypt(contend.as_bytes(), &private_data.user_key);
