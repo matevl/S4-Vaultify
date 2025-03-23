d073c83817f8fb0d449b5e8725c0819062a37c07
swann.charlery-fontes <101474803+agrownboy@users.noreply.github.com>
2025-03-23 15:09:47 +0100

    ca fonctionne pas

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,123 +1,96 @@
-use actix_files as fs; // Import pour servir des fichiers statiques
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use lazy_static::lazy_static;
-use s4_vaultify::backend::account_manager::account::{
-    add_user_to_data, load_users_data, load_vault_matching, log_to_vaultify, UserData, UserInput,
-    JWT,
-};
-use s4_vaultify::backend::VAULTIFY_CONFIG;
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use actix_session::{CookieSession, Session};
+use actix_web::middleware::Logger;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-
-lazy_static! {
-    pub static ref USERD: Mutex<Vec<UserData>> = Mutex::new(load_users_data());
-    static ref VAULT_MATCh: Mutex<HashMap<String, Vec<(String, String)>>> =
-        Mutex::new(load_vault_matching());
-}
+use lazy_static::lazy_static;
+use bcrypt::{hash, verify, DEFAULT_COST};
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize)]
-struct LogInput {
+struct UserJson {
     email: String,
     password: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct CreateUserResponse {
+#[derive(Serialize, Deserialize, Clone)]
+struct JWT {
     id: u32,
-    name: String,
-    wuser_data: WuserData,
+    email: String,
 }
 
-#[derive(Serialize, Deserialize)]
-struct WuserData {
-    id: u32,
-    user_input: UserInput,
-    jwt: JWT,
-}
+type UsersData = HashMap<String, (String, u32)>;
 
-impl WuserData {
-    pub(crate) fn clone(&self) -> WuserData {
-        WuserData {
-            id: self.id,
-            user_input: self.user_input.clone(),
-            jwt: self.jwt.clone(),
-        }
-    }
+lazy_static! {
+    static ref USERS_DB: Arc<Mutex<UsersData>> = Arc::new(Mutex::new(HashMap::new()));
 }
 
 #[actix_web::post("/user/register")]
-async fn create_user(user_data: web::Json<UserInput>, db: web::Data<UserDb>) -> impl Responder {
-    let mut db = db.lock().unwrap();
-    let new_id = db.keys().max().unwrap_or(&0) + 1;
-    let name = user_data.email.clone();
-    let res = add_user_to_data(&user_data, &mut *USERD.lock().unwrap());
-    let jwt = log_to_vaultify(&user_data, &mut *USERD.lock().unwrap())
-        .expect("Error while adding user to vaultify");
-    let wuser_data = WuserData {
-        id: new_id,
-        user_input: user_data.clone(),
-        jwt,
-    };
-    db.insert(new_id, wuser_data.clone());
-    HttpResponse::Created().json(CreateUserResponse {
-        id: new_id,
-        name,
-        wuser_data,
-    })
+async fn create_user_query(user: web::Json<UserJson>, session: Session) -> impl Responder {
+    let email = user.email.clone();
+    let pw = user.password.clone();
+
+    let mut db = USERS_DB.lock().unwrap();
+    let new_id = db.values().last().map_or(1, |last_user| last_user.1 + 1);
+
+    let hash_pw = hash(pw, DEFAULT_COST).unwrap();
+    db.insert(email.clone(), (hash_pw, new_id));
+
+    session.insert("user_id", new_id).unwrap();
+
+    HttpResponse::Ok().json(JWT { id: new_id, email })
 }
 
-#[actix_web::post("/user/log")]
-async fn log_user(user_data: web::Json<LogInput>, db: web::Data<UserDb>) -> impl Responder {
-    let mut db = db.lock().unwrap();
-    let new_id = db.keys().max().unwrap_or(&0) + 1;
-    let mail = user_data.email.clone();
-    let password = user_data.password.clone();
-    let usrinput = UserInput::new(mail.clone(), password.clone());
-    let jwt = log_to_vaultify(&usrinput, &mut *USERD.lock().unwrap())
-        .expect("Error while adding user to vaultify");
-    db.insert(
-        new_id,
-        WuserData {
-            id: new_id,
-            user_input: usrinput.clone(),
-            jwt: jwt.clone(),
-        },
-    );
-    HttpResponse::Created().json(CreateUserResponse {
-        id: new_id,
-        name: mail,
-        wuser_data: WuserData {
-            id: new_id,
-            user_input: usrinput.clone(),
-            jwt: jwt.clone(),
-        },
-    })
+#[actix_web::post("/user/login")]
+async fn login_user_query(user: web::Json<UserJson>, session: Session) -> impl Responder {
+    let email = user.email.clone();
+    let pw = user.password.clone();
+    let db = USERS_DB.lock().unwrap();
+
+    if let Some((hash_pw, user_id)) = db.get(&email) {
+        if verify(&pw, hash_pw).is_ok() {
+            session.insert("user_id", *user_id).unwrap();
+            return HttpResponse::Ok().json(JWT { id: *user_id, email });
+        }
+    }
+
+    HttpResponse::Unauthorized().finish()
 }
 
-type UserDb = Arc<Mutex<HashMap<u32, WuserData>>>;
-#[actix_web::get("/greet/{name}")]
-async fn greet(user_id: web::Path<u32>) -> impl Responder {
-    format!("Hello {user_id}")
+#[actix_web::get("/home")]
+async fn home(session: Session) -> impl Responder {
+    if let Some(user_id) = session.get::<u32>("user_id").unwrap() {
+        HttpResponse::Ok().body(format!("Bienvenue sur la page d'accueil, utilisateur {}", user_id))
+    } else {
+        HttpResponse::Found()
+            .header("LOCATION", "/login")
+            .finish()
+    }
+}
+
+#[actix_web::get("/login")]
+async fn login_page() -> impl Responder {
+    HttpResponse::Ok().body(include_str!("../../static/login.html"))
+}
+
+#[actix_web::get("/register")]
+async fn register_page() -> impl Responder {
+    HttpResponse::Ok().body(include_str!("../../static/register.html"))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = 8080;
-    let user_db: UserDb = Arc::new(Mutex::new(HashMap::<u32, WuserData>::new()));
-    println!("Starting server on port {}", port);
-    HttpServer::new(move || {
-        let app_data = web::Data::new(user_db.clone());
+    HttpServer::new(|| {
         App::new()
-            .app_data(app_data)
-            .service(create_user)
-            .service(log_user)
-            .service(greet)
-            // Servir les fichiers statiques depuis le dossier "static"
-            .service(fs::Files::new("/", "./static").index_file("index.html"))
+            .wrap(CookieSession::signed(&[0; 32]).secure(false))
+            .wrap(Logger::default())
+            .service(create_user_query)
+            .service(login_user_query)
+            .service(home)
+            .service(login_page)
+            .service(register_page)
     })
-        .bind(format!("127.0.0.1:{}", port))?
-        .workers(2)
+        .bind("127.0.0.1:8080")?
         .run()
         .await
 }
