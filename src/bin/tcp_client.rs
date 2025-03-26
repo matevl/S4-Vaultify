a964d6ada6358c1525d1c9fc8d94c251d9c1c4de
Lothaire07 <lothaire.chacornac@epita.fr>
2025-03-26 19:08:05 +0100

    fmt

--- src/bin/tcp_client.rs
+++ src/bin/tcp_client.rs
@@ -2,5 +2,7 @@ use s4_vaultify::backend::file_flow::send_to::send;
 
 #[tokio::main]
 async fn main() {
-    send("/Users/lothaire/RustroverProjects/S4-Vaultify/test-files/IMG_1204.HEIC").await.expect("TODO: panic message");
+    send("/Users/lothaire/RustroverProjects/S4-Vaultify/test-files/IMG_1204.HEIC")
+        .await
+        .expect("TODO: panic message");
 }
