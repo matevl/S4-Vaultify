25670e0ef9f10646cd04a8ccb912d3eb89cd0461
matevl <144525314+matevl@users.noreply.github.com>
2025-03-27 14:58:52 +0100

    almost there

--- src/backend/account_manager/account_server.rs
+++ src/backend/account_manager/account_server.rs
@@ -1,22 +1,29 @@
+use crate::backend::aes_keys::crypted_key::encrypt;
+use crate::backend::aes_keys::decrypted_key::decrypt;
+use crate::backend::aes_keys::keys_password::{
+    derive_key, generate_random_key, generate_salt_from_login,
+};
+use crate::backend::{
+    VAULTIFY_CONFIG, VAULTIFY_DATABASE, VAULTS_DATA, VAULT_CONFIG_ROOT, VAULT_USERS_DIR,
+};
 use actix_web::{web, HttpResponse, Responder};
 use bcrypt::{hash, verify, DEFAULT_COST};
-use serde::{Deserialize, Serialize};
+use dirs;
+use lazy_static::lazy_static;
 use rusqlite::{params, Connection, Result};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
-use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use std::time::{SystemTime, UNIX_EPOCH, Duration};
-use lazy_static::lazy_static;
-use dirs;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use uuid::Uuid;
-use crate::backend::{VAULTIFY_CONFIG, VAULTIFY_DATABASE, VAULTS_DATA};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Perms {
     Admin,
     Write,
     Read,
+    NoLoad,
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -38,26 +45,54 @@ pub struct VaultInfo {
     pub date: u64,
 }
 
+impl VaultInfo {
+    pub fn new(name: &str, path: &str, date: u64) -> Self {
+        Self {
+            name: name.to_string(),
+            path: path.to_string(),
+            date,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct JWT {
-    pub id: i32,
+    pub id: u32,
     pub email: String,
     pub loaded_vault: Option<VaultInfo>,
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct JWTPrivate {
-    hash_pw: String,
-    user_key: Vec<u8>,
-    vault_key: Vec<u8>,
-    vault_perms: Perms,
+impl JWT {
+    pub fn new(id: u32, email: &str) -> Self {
+        Self {
+            id,
+            email: email.to_string(),
+            loaded_vault: None,
+        }
+    }
 }
 
 #[derive(Clone)]
 pub struct Session {
-    pub user_id: i32,
-    pub private_data: JWTPrivate,
+    pub user_id: u32,
+    pub hash_pw: String,
+    pub user_key: Vec<u8>,
+    pub vault_key: Vec<u8>,
+    pub vault_perms: Perms,
     pub last_activity: SystemTime,
+}
+
+impl Session {
+    pub fn new(user_id: u32, hash_pw: &str, user_key: &[u8]) -> Self {
+        Self {
+            user_id,
+            hash_pw: hash_pw.to_string(),
+            user_key: user_key.to_vec(),
+            vault_key: vec![],
+            vault_perms: Perms::NoLoad,
+            last_activity: SystemTime::now(),
+        }
+    }
 }
 
 lazy_static! {
@@ -70,6 +105,8 @@ lazy_static! {
      * Global cache for user sessions.
      */
     pub static ref SESSION_CACHE: Arc<Mutex<HashMap<String, Session>>> = Arc::new(Mutex::new(HashMap::new()));
+
+    pub static ref CONNECTION: Arc<Mutex<Connection>> = Arc::new(Mutex::new(init_db_connection(VAULTIFY_DATABASE).unwrap()));
 }
 
 // Function to initialize the connection to the database
@@ -78,16 +115,16 @@ pub fn init_db_connection(database_path: &str) -> Result<Connection> {
 }
 
 // Function to create a new user
-pub fn create_user(conn: &Connection, email: &str, hash_password: &str) -> Result<i32> {
+pub fn create_user(conn: &Connection, email: &str, hash_password: &str) -> Result<u32> {
     conn.execute(
         "INSERT INTO users (email, hash_password) VALUES (?, ?)",
         params![email, hash_password],
     )?;
-    Ok(conn.last_insert_rowid() as i32)
+    Ok(conn.last_insert_rowid() as u32)
 }
 
 // Function to get a user by email
-pub fn get_user_by_email(conn: &Connection, email: &str) -> Result<Option<(i32, String)>> {
+pub fn get_user_by_email(conn: &Connection, email: &str) -> Result<Option<(u32, String)>> {
     let mut stmt = conn.prepare("SELECT id, hash_password FROM users WHERE email = ?")?;
     let mut rows = stmt.query(params![email])?;
     if let Some(row) = rows.next()? {
@@ -98,16 +135,22 @@ pub fn get_user_by_email(conn: &Connection, email: &str) -> Result<Option<(i32, 
 }
 
 // Function to create a new vault for a user
-pub fn create_vault(conn: &Connection, user_id: i32, name: &str, path: &str, date: u64) -> Result<i32> {
+pub fn create_vault(
+    conn: &Connection,
+    user_id: u32,
+    name: &str,
+    path: &str,
+    date: u64,
+) -> Result<u32> {
     conn.execute(
         "INSERT INTO vaults (user_id, name, path, date) VALUES (?, ?, ?, ?)",
         params![user_id, name, path, date as i64],
     )?;
-    Ok(conn.last_insert_rowid() as i32)
+    Ok(conn.last_insert_rowid() as u32)
 }
 
 // Function to get the vaults of a user
-pub fn get_user_vaults(conn: &Connection, user_id: i32) -> Result<Vec<VaultInfo>> {
+pub fn get_user_vaults(conn: &Connection, user_id: u32) -> Result<Vec<VaultInfo>> {
     let mut stmt = conn.prepare("SELECT name, path, date FROM vaults WHERE user_id = ?")?;
     let mut rows = stmt.query(params![user_id])?;
     let mut vaults = Vec::new();
@@ -137,88 +180,94 @@ fn clean_expired_sessions() {
 }
 
 // Endpoint to create a new user
-pub async fn create_user_query(
-    conn: web::Data<Connection>,
-    form: web::Form<CreateUserForm>,
-) -> impl Responder {
+pub async fn create_user_query(form: web::Form<CreateUserForm>) -> impl Responder {
+    let conn = CONNECTION.lock().unwrap();
     let email = form.username.clone();
     let pw = form.password.clone();
-    let hash_pw = hash(pw, DEFAULT_COST).unwrap();
+    let hash_pw = hash(&pw, DEFAULT_COST).unwrap();
     let user_id = create_user(&conn, &email, &hash_pw).unwrap();
-    HttpResponse::Ok().json(JWT {
-        id: user_id,
-        email: email.clone(),
-        loaded_vault: None,
-    })
+
+    let user_key = derive_key(&pw, &generate_salt_from_login(&email), 10000);
+    let session_id = generate_session_id();
+    SESSION_CACHE.lock().unwrap().insert(
+        session_id.clone(),
+        Session::new(user_id, &hash_pw, &user_key),
+    );
+
+    HttpResponse::Ok()
 }
 
-// Endpoint to log in a user
-pub async fn login_user_query(
-    conn: web::Data<Connection>,
-    form: web::Form<LoginForm>,
-) -> impl Responder {
+pub async fn login_user_query(form: web::Form<LoginForm>) -> impl Responder {
+    let conn = CONNECTION.lock().unwrap();
     let email = form.username.clone();
     let pw = form.password.clone();
-    if let Some((user_id, hash_pw)) = get_user_by_email(&conn, &email).unwrap(){
-        if verify(&pw, &hash_pw).is_ok() {
+    if let Some((user_id, hash_pw)) = get_user_by_email(&conn, &email).unwrap() {
+        if verify(&pw, &hash_pw).unwrap() {
+            let user_key = derive_key(&pw, &generate_salt_from_login(&email), 10000);
             let session_id = generate_session_id();
-            let private_data = JWTPrivate {
-                hash_pw: hash_pw.to_string(),
-                user_key: vec![],  // Replace with the actual user key
-                vault_key: vec![], // Replace with the actual vault key
-                vault_perms: Perms::Read,
-            };
-            let session = Session {
-                user_id,
-                private_data,
-                last_activity: SystemTime::now(),
-            };
-            let mut cache = SESSION_CACHE.lock().unwrap();
-            cache.insert(session_id.clone(), session);
-            return HttpResponse::Ok().json(JWT {
-                id: user_id,
-                email: email.clone(),
-                loaded_vault: None,
-            });
+            SESSION_CACHE.lock().unwrap().insert(
+                session_id.clone(),
+                Session::new(user_id, &hash_pw, &user_key),
+            );
+            return HttpResponse::Ok().json(JWT::new(user_id, &email));
         }
     }
-    HttpResponse::NotFound().finish()
+    HttpResponse::Unauthorized().finish()
 }
 
 // Endpoint to get the list of vaults for a user
-pub async fn get_vaults_list_query(
-    conn: web::Data<Connection>,
-    user: web::Json<JWT>,
-) -> impl Responder {
+pub async fn get_vaults_list_query(user: web::Json<JWT>) -> impl Responder {
+    let conn = CONNECTION.lock().unwrap();
     let vaults = get_user_vaults(&conn, user.id).unwrap();
     HttpResponse::Ok().json(vaults)
 }
 
 // Endpoint to create a new vault for a user
 pub async fn create_vault_query(
-    conn: web::Data<Connection>,
     mut jwt: web::Json<JWT>,
     name: web::Json<String>,
 ) -> impl Responder {
-    let time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    let vault_path = format!(
-        "{}/{}{}",
-        ROOT.join(VAULTS_DATA).to_str().unwrap(),
-        jwt.id,
-        time
-    );
-    fs::create_dir_all(&vault_path).unwrap();
-    let vault_id = create_vault(&conn, jwt.id, &name, &vault_path, time).unwrap();
-    let info = VaultInfo {
-        name: name.into_inner(),
-        path: vault_path,
-        date: time,
-    };
-    jwt.loaded_vault = Some(info.clone());
-    HttpResponse::Ok().json(jwt)
+    let sessions = SESSION_CACHE.lock().unwrap();
+    if let Some(cache) = sessions.get(&jwt.email) {
+        let time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let vault_path = format!(
+            "{}/{}{}/{}/",
+            ROOT.to_str().unwrap(),
+            VAULTS_DATA,
+            jwt.id,
+            time
+        );
+
+        let vault_config = format!("{}{}", vault_path, VAULT_CONFIG_ROOT);
+        let users_vault = format!("{}{}", vault_config, VAULT_USERS_DIR);
+        let user_json = format!("{users_vault}{}.json", jwt.id);
+
+        fs::create_dir_all(&vault_path).unwrap();
+        fs::create_dir_all(&vault_config).unwrap();
+        fs::create_dir_all(&users_vault).unwrap();
+        fs::File::create(&user_json).unwrap();
+
+        let info = VaultInfo::new(&name, &vault_path, time);
+
+        let vault_key = generate_random_key();
+
+        let vault_key = derive_key(
+            &String::from_utf8(vault_key).unwrap(),
+            generate_salt_from_login(&jwt.email).as_slice(),
+            10000,
+        );
+
+        let content = serde_json::to_string(&(vault_key, Perms::Admin)).unwrap();
+        let encrypted_content = encrypt(content.as_bytes(), cache.user_key.as_slice());
+        fs::write(&user_json, &encrypted_content).unwrap();
+        HttpResponse::Ok().json(jwt)
+    } else {
+        HttpResponse::ExpectationFailed().finish()
+    }
 }
 
 // Endpoint to load a vault for a user
@@ -227,35 +276,39 @@ pub async fn load_vault_query(
     mut jwt: web::Json<JWT>,
     info: web::Json<VaultInfo>,
 ) -> impl Responder {
-    // Logic to load a vault
-    jwt.loaded_vault = Some(info.clone());
-    HttpResponse::Ok().json(jwt)
+    if let Some(cache) = SESSION_CACHE.lock().unwrap().get_mut(&jwt.email) {
+        let key_path = format!("{}{}{}.json", info.path, VAULTS_DATA, jwt.id);
+
+        let encrypted_content = fs::read_to_string(&key_path).unwrap();
+        let decrypted_content =
+            decrypt(encrypted_content.as_bytes(), cache.user_key.as_slice()).unwrap();
+        let (vault_key, vault_perms): (Vec<u8>, Perms) =
+            serde_json::from_str(&String::from_utf8(decrypted_content).unwrap()).unwrap();
+        cache.vault_key = vault_key;
+        cache.vault_perms = vault_perms;
+
+        jwt.loaded_vault = Some(info.clone());
+        HttpResponse::Ok().json(jwt)
+    } else {
+        HttpResponse::ExpectationFailed().finish()
+    }
 }
 
-pub async fn init_server_config(root: &PathBuf) {
-    // Construct the path for the configuration directory
-    let config_path = root.join(VAULTIFY_CONFIG);
-    // Create the configuration directory if it doesn't exist
+pub fn init_server_config() {
+    let config_path = ROOT.join(VAULTIFY_CONFIG);
     fs::create_dir_all(&config_path).unwrap_or_else(|why| {
-        eprintln!(
-            "Error creating the configuration directory: {:?}",
-            why
-        );
+        eprintln!("Error creating the configuration directory: {:?}", why);
     });
-    // Construct the path for the database
-    let database_path = root.join(VAULTIFY_DATABASE);
-    // Create the directory for the database if it doesn't exist
+
+    let database_path = ROOT.join(VAULTIFY_DATABASE);
     if let Some(parent_dir) = database_path.parent() {
         fs::create_dir_all(parent_dir).unwrap_or_else(|why| {
-            eprintln!(
-                "Error creating the directory for the database: {:?}",
-                why
-            );
+            eprintln!("Error creating the directory for the database: {:?}", why);
         });
     }
-    // Initialize the connection to the database
+
     let conn = init_db_connection(database_path.to_str().unwrap()).unwrap();
-    // Initialize the tables if they don't exist
+
     conn.execute(
         "CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -263,7 +316,9 @@ pub async fn init_server_config(root: &PathBuf) {
             hash_password TEXT NOT NULL
         )",
         [],
-    ).unwrap();
+    )
+    .unwrap();
+
     conn.execute(
         "CREATE TABLE IF NOT EXISTS vaults (
             vault_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -274,5 +329,6 @@ pub async fn init_server_config(root: &PathBuf) {
             FOREIGN KEY (user_id) REFERENCES users(id)
         )",
         [],
-    ).unwrap();
+    )
+    .unwrap();
 }
--- src/bin/main_server.rs
+++ src/bin/main_server.rs
@@ -1,14 +1,12 @@
 use actix_files::NamedFile;
-use actix_web::{web, App, HttpServer, HttpResponse, Responder};
-use s4_vaultify::backend::account_manager::account_server::{
-    create_user_query, login_user_query, get_vaults_list_query, create_vault_query, load_vault_query, init_server_config, init_db_connection
-};
-use s4_vaultify::backend::{VAULTIFY_CONFIG, VAULTIFY_DATABASE, VAULTS_DATA};
-use std::fs;
-use std::path::PathBuf;
-use std::sync::Arc;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use lazy_static::lazy_static;
-use std::env;
+use s4_vaultify::backend::account_manager::account_server::{
+    create_user_query, create_vault_query, get_vaults_list_query, init_db_connection,
+    init_server_config, load_vault_query, login_user_query,
+};
+use s4_vaultify::backend::VAULTIFY_DATABASE;
+use std::sync::Arc;
 
 // Gestion des formulaires (POST)
 
@@ -35,47 +33,27 @@ async fn login_page() -> actix_web::Result<NamedFile> {
     Ok(NamedFile::open("./templates/login.html")?)
 }
 
-lazy_static! {
-    /**
-     * Root directory path for the application.
-     */
-    pub static ref ROOT: std::path::PathBuf = dirs::home_dir().expect("Could not find home dir");
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let port = 8080;
     println!("Starting server on port {}", port);
 
-    // Construct the path for the database
-    let database_path = ROOT.join(VAULTIFY_DATABASE);
+    init_server_config();
 
-    // Initialize the server configuration
-    init_server_config(&ROOT).await;
-
-    // Initialize the connection to the database
-    let conn = init_db_connection(database_path.to_str().unwrap()).unwrap();
-
-    // Start the HTTP server
     HttpServer::new(move || {
         App::new()
-            // Add the connection to the application context
-            .app_data(web::Data::new(conn.clone()))
-            // Routes for displaying HTML
-            .route("/create-user", web::get().to(create_user_page)) // Displays the user creation page
-            .route("/login", web::get().to(login_page)) // Displays the login page
-            // Routes for API POST requests
-            .route("/create-user", web::post().to(create_user_query)) // Handles the user creation form
-            .route("/login", web::post().to(login_user_query)) // Handles the login form
-            .route("/vaults", web::get().to(get_vaults_list_query)) // Handles the list of vaults for a user
-            .route("/create-vault", web::post().to(create_vault_query)) // Handles the creation of a new vault
-            .route("/load-vault", web::post().to(load_vault_query)) // Handles the loading of a vault
-            // Static files
+            .route("/create-user", web::get().to(create_user_page))
+            .route("/login", web::get().to(login_page))
+            .route("/create-user", web::post().to(create_user_query))
+            .route("/login", web::post().to(login_user_query))
+            .route("/vaults", web::get().to(get_vaults_list_query))
+            .route("/create-vault", web::post().to(create_vault_query))
+            .route("/load-vault", web::post().to(load_vault_query))
             .service(actix_files::Files::new("/static", "./static").show_files_listing())
             .service(actix_files::Files::new("/", "./templates").index_file("index.html"))
     })
-        .bind(format!("127.0.0.1:{}", port))?
-        .workers(2)
-        .run()
-        .await
+    .bind(format!("127.0.0.1:{}", port))?
+    .workers(4)
+    .run()
+    .await
 }
