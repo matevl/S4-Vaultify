d544e08e7da87a0a06cdc0347e21a895ecf14e44
Lothaire07 <lothaire.chacornac@epita.fr>
2025-03-31 07:38:08 +0200

    fix

--- src/bin/tcp_client.rs
+++ src/bin/tcp_client.rs
@@ -3,6 +3,5 @@ use s4_vaultify::backend::file_flow::send_to::send;
 #[tokio::main]
 async fn main() {
     send("/Users/lothaire/RustroverProjects/S4-Vaultify/test-files/IMG_1204.HEIC")
-        .await
-        .expect("TODO: panic message");
+        .await.expect("TODO: panic message");
 }
