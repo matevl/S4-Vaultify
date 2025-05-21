720d56a6cc1417db3856c261a7c6fd3e0cb51c3e
Lothaire07 <lothaire.chacornac@epita.fr>
2025-05-21 17:02:52 +0200

    logout toast

--- frontend/Index.jsx
+++ frontend/Index.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
 export default function Index() {
+    const [showLogoutToast, setShowLogoutToast] = useState(false);
+
+    useEffect(() => {
+        if (localStorage.getItem('logoutSuccess') === 'true') {
+            setShowLogoutToast(true);
+            localStorage.removeItem('logoutSuccess');
+            setTimeout(() => setShowLogoutToast(false), 3000);
+        }
+    }, []);
+
     return (
         <div className="index-page">
             <div className="animated-bg"></div>
@@ -19,7 +29,21 @@ export default function Index() {
                     <a href="/about" className="about-link">About us</a>
                     <a href="https://github.com/matevl/S4-Vaultify" className="about-link">Github</a>
                 </div>
+
             </div>
+            {showLogoutToast && (
+                <div className="toast-notification">
+                    <span className="toast-icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                             className="icon-size">
+                            <path strokeLinecap="round" strokeLinejoin="round"
+                                  d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </span>
+                    <span>Successful logout</span>
+                </div>
+            )}
         </div>
     );
 }
